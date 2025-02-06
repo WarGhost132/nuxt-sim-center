6564ca393f3512a540b78b84a1cb77b405d5bdfc
WarGhost132 <wawghostick@gmail.com>
2025-02-06 18:09:57 +0300

    add badge, theme switcher button

---
+++ app/components/common/badge/UiBadge.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  badgeStatus: string,
+}>()
+
+const status = computed(() => {
+  switch (props.badgeStatus) {
+    case 'Запланировано':
+      return 'bg-[var(--color-badge-planned)]'
+    case 'Идет':
+      return 'bg-[var(--color-badge-proccess)]'
+    case 'Завершено':
+      return 'bg-[var(--color-badge-done)]'
+    default:
+      return ''
+  }
+})
+</script>
+
+<template>
+  <span class="px-3 py-1 rounded-3xl text-[var(--color-text)]" :class="status">
+    {{ badgeStatus }}
+  </span>
+</template>
